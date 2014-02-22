670c6f0992461069f47d5d58f738b68421b1c5a0
EilidhHendry <ehendry91@gmail.com>
2014-02-22 16:29:27 +0000

    gitignore videos

--- IVRvideoread.m
+++ IVRvideoread.m
@@ -1,8 +1,8 @@
 
-file_dir = 'Video1'; %put here one of the folder locations with images;
+file_dir = '~/year_3/ivr/Video1'; %put here one of the folder locations with images;
 filenames = dir([file_dir '*.jpg']);
 
-frame = imread([file_dir filenames(1).name]);
+frame = imread([file_dir filenames().name]);
 figure(1); h1 = imshow(frame);
 
 % Read one frame at a time.
