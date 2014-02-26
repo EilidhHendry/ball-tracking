c7ef8fc492ceeb25412cd39b083126107e7ab36c
EilidhHendry <ehendry91@gmail.com>
2014-02-26 19:31:16 +0000

    IVRvideoread.m now uses hsv. Moved grayscale to IVRvideoreadgray.m.

--- IVRvideoreadgray.m
+++ IVRvideoreadgray.m
@@ -1,8 +1,7 @@
 file_dir = 'Video1/'; %put here one of the folder locations with images;
 filenames = dir([file_dir '*.jpg']);
 
-%frame = myrgb2gray(imread([file_dir filenames(1).name]));
-frame = rgb2hsv(imread([file_dir filenames(1).name]));
+frame = myrgb2gray(imread([file_dir filenames(1).name]));
 figure(1); h1 = imshow(frame);
 
 edges = zeros(256,1);
@@ -13,30 +12,24 @@ edges = zeros(256,1);
 show = 1;
 
 bckgrnd = imread([file_dir filenames(5).name]);
-
-%gray:
 %bckgrnd = normalize(bckgrnd);                   
-%grayBG = myrgb2gray(bckgrnd);              
-%grayBG = double (grayBG);
+grayBG = myrgb2gray(bckgrnd);              
+grayBG = double (grayBG);
 
-%hsv:
-bckgrnd = normalize(bckgrnd);
-hsvBG = rgb2hsv(bckgrnd);
-hsvBG = double (hsvBG);
 
 % Plotting the histogram:
 
-% [R,C] = size(hsvBG);
-% bckgrndVec = reshape(bckgrnd,1,R*C);      % turn image into long array
-% hist = histc(bckgrndVec,edges)';        % do histogram
-%   if show > 0
-%       figure(show)
-%       clf
-%       pause(0.1)
-%       plot(edges,hist)
-%       axis([0, 255, 0, 1.1*max(hist)])
-%       imshow(before)
-%   end
+ %[R,C] = size(bckgrnd);
+ %bckgrndVec = reshape(bckgrnd,1,R*C);      % turn image into long array
+ %hist = histc(bckgrndVec,edges)';        % do histogram
+ %  if show > 0
+ %      figure(show)
+ %      clf
+ %      pause(0.1)
+ %      plot(edges,hist)
+ %      axis([0, 255, 0, 1.1*max(hist)])
+ %      imshow(before)
+ %  end
 
 % Read one frame at a time.
 for k = 1 : size(filenames,1)
@@ -45,22 +38,22 @@ for k = 1 : size(filenames,1)
     frame = imread([file_dir filenames(k).name]);
     
     % Normalization  - this slows down the prgram a lot
-     frame = normalize(frame);
+%     frame = normalize(frame);
     
-    %HSV:
-    hsvframe = rgb2hsv(frame);
-    hsvframe = double(hsvframe);
-
+    grayframe = myrgb2gray(frame);
+    grayframe = double(grayframe);
+    frameVec = reshape(grayframe,1,480*640);
+    
     % Divide image with background frame
-    frameDiff = hsvframe./hsvBG;
+    frameDiff = grayframe./grayBG;
     
     % Object/background relative brightness threshold
-    thresh = 0.4;
+    thresh = 0.8;
     
     object = (frameDiff <= thresh);
+ %   object = reshape(object,480,640);
+ %   object = 255*object;
     
-    %change=abs(hsvBG-hsvframe);
-        
     set(h1, 'CData', object);
     drawnow('expose');
     disp(['showing frame ' num2str(k)]);
