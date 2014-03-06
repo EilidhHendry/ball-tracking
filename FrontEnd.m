7c20b08f9b22a0fa50b133fd2cd3274763eac8fb
EilidhHendry <ehendry91@gmail.com>
2014-03-06 11:52:16 +0000

    moving detectballs to own function

--- FrontEnd.m
+++ FrontEnd.m
@@ -16,7 +16,8 @@ path1 = zeros(500,2);
 path2 = zeros(500,2);
 
 highestReached1=0;
-highestReached2=0;
+highestReached2=0;      % 0/1 for highest point reached or not
+
 highestPoint1=[5 5];
 highestPoint2=[5 5];    % initialise highest point
 
@@ -60,44 +61,7 @@ for k = 250 : size(filenames,1)
     
     % Ball detection 
     
-    ballFinder = regionprops(binaryImage2D,'Area','BoundingBox', ...
-        'MajorAxisLength', 'MinorAxisLength', 'ConvexArea', 'Solidity');
-    itemProps = struct2cell(ballFinder);
-    areas = cell2mat(itemProps(1,:));
-    boxes = zeros(10,4);
-    for i=1 : length(itemProps(1,:))
-        boxes(i,:) = cell2mat(itemProps(2,i));
-    end    
-    bigAxes = cell2mat(itemProps(3,:));
-    smallAxes = cell2mat(itemProps(4,:));
-    convexArea = cell2mat(itemProps(5,:));
-    solidities = cell2mat(itemProps(6,:));
-    
-    objects = zeros(2,5,4);
-    
-    count = 1;
-    
-    for i=1 : length(itemProps(1,:))
-        if areas(i) > 100            
-            objects(count,1,:) = areas(i);
-            objects(count,2,:) = boxes(i,:);            
-            objects(count,3,:) = bigAxes(i);
-            objects(count,4,:) = smallAxes(i);
-            objects(count,5,:) = convexArea(i);
-            objects(count,6,:) = solidities(i);
-            count = count + 1;
-        end        
-    end
-    
-    % Iterate over number of objects in image
-    for i=1 : length(find(objects(:,1,1)))
-        axisRatio = objects(i,4,1) / objects(i,3,1);
-        convexAreaRatio = objects(i,1,1) / objects(i,5,1);
-        if objects(i,6,1) > 0.94 && axisRatio > 0.67 ...
-                && convexAreaRatio > 0.95
-            frame = drawRect(frame,objects(i,2,:), 0, 0, 255);
-        end
-    end
+    frame = detectBalls(binaryImage2D, frame);
     
     % Uncomment to display binary image:
     frame = frame ./ 255;
